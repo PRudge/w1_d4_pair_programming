c44c1f4bdd80b44d0e07c2c65fe1e2dfdbe174c2
Pauline Rudge <paulinerudge@me.com>
2019-01-17 14:09:21 +0000

    2nd test, pass

--- friends.rb
+++ friends.rb
@@ -5,3 +5,11 @@ end
 def get_tvshow(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, snack)
+  for snck in person[:favourites][:snacks]
+    if snck == snack
+      return true
+    end
+  end
+end
